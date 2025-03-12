d623fa5b887ff792ff010176edd7afb09d727e84
Deepa lamichhane <deepa.lamichhane@thamescollege.edu.np>
2025-03-12 22:28:28 +0545

    added authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const authRoute = require("./routes/auth");
+module.exports = app;
 
+//CORS
+app.use(cors());
 
+//JSON
+app.use(express.json());
 
-module.exports=app;
+//ROUTES
+app.use("/api/v1/auth", authRoute);
